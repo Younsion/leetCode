66bc2b383712a105f7fdbfbe45ced4ae58ad3e79
yongxing.chen <yongxing.chen@qunar.com>
2018-03-30 18:25:54 +0800

    5-6

--- src/main/java/com/leetcode/problems/p1_10/P5LongestPalindromic.java
+++ src/main/java/com/leetcode/problems/p1_10/P5LongestPalindromic.java
@@ -6,6 +6,47 @@ package com.leetcode.problems.p1_10;
  */
 public class P5LongestPalindromic {
     public String longestPalindrome(String s) {
-        return null;
+        if(s == null || s.length() == 0) {
+            return "";
+        }
+        String lpd = s.substring(0, 1);
+        int left = -1, index = 0, right = 1;
+        for(int i = 0; i < s.length() - 1; i++) {
+            index = i;
+            if(s.charAt(i) == s.charAt(i + 1)) {
+                for(int j = i + 1; j < s.length(); j++) {
+                    if(s.charAt(i) != s.charAt(j)) {
+                        break;
+                    }
+                    index = j;
+                    lpd = lpd.length() < j - i + 1 ? s.substring(i, j + 1) : lpd;
+                }
+            }
+            left = i - 1;
+            right = index + 1;
+            i = index;
+            while (left > -1 && right < s.length()) {
+                if(s.charAt(left) == s.charAt(right)) {
+                    lpd = lpd.length() < right - left + 1 ? s.substring(left, right + 1) : lpd;
+                    left--;
+                    right++;
+                    continue;
+                }
+                break;
+            }
+        }
+        return lpd;
+    }
+
+    public String reversString(String s) {
+        char[] reversS2C = new char[s.length()];
+        for(int i = s.length(); i > 0; i--) {
+            reversS2C[s.length() - i] = s.charAt(i - 1);
+        }
+        return new String(reversS2C);
+    }
+
+    public static void main(String[] args) {
+        new P5LongestPalindromic().longestPalindrome("rgczcpratwyqxaszbuwwcadruayhasynuxnakpmsyhxzlnxmdtsqqlmwnbxvmgvllafrpmlfuqpbhjddmhmbcgmlyeypkfpreddyencsdmgxysctpubvgeedhurvizgqxclhpfrvxggrowaynrtuwvvvwnqlowdihtrdzjffrgoeqivnprdnpvfjuhycpfydjcpfcnkpyujljiesmuxhtizzvwhvpqylvcirwqsmpptyhcqybstsfgjadicwzycswwmpluvzqdvnhkcofptqrzgjqtbvbdxylrylinspncrkxclykccbwridpqckstxdjawvziucrswpsfmisqiozworibeycuarcidbljslwbalcemgymnsxfziattdylrulwrybzztoxhevsdnvvljfzzrgcmagshucoalfiuapgzpqgjjgqsmcvtdsvehewrvtkeqwgmatqdpwlayjcxcavjmgpdyklrjcqvxjqbjucfubgmgpkfdxznkhcejscymuildfnuxwmuklntnyycdcscioimenaeohgpbcpogyifcsatfxeslstkjclauqmywacizyapxlgtcchlxkvygzeucwalhvhbwkvbceqajstxzzppcxoanhyfkgwaelsfdeeviqogjpresnoacegfeejyychabkhszcokdxpaqrprwfdahjqkfptwpeykgumyemgkccynxuvbdpjlrbgqtcqulxodurugofuwzudnhgxdrbbxtrvdnlodyhsifvyspejenpdckevzqrexplpcqtwtxlimfrsjumiygqeemhihcxyngsemcolrnlyhqlbqbcestadoxtrdvcgucntjnfavylip");
     }
 }
---
+++ src/main/java/com/leetcode/problems/p1_10/P6ZigzagConversion.java
@@ -0,0 +1,18 @@
+package com.leetcode.problems.p1_10;
+
+/**
+ * @author yongxing.chen
+ * @create 2018-03-30 下午3:55
+ */
+public class P6ZigzagConversion {
+    public String convert(String s, int numRows) {
+        char[][] chars = new char[numRows][];
+        for(int i = 0; i< numRows; i++) {
+
+        }
+        return null;
+    }
+
+    public static void main(String[] args) {
+    }
+}
