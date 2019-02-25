3b443740b4490cda06e4c1a73467728bf9c2e21a
yongxing.chen <yongxing.chen@qunar.com>
2019-02-25 20:32:20 +0800

    int[] plusOne(int[] digits);

--- src/main/java/com/leetcode/problems/exploreAlgorithm/array/Solution.java
+++ src/main/java/com/leetcode/problems/exploreAlgorithm/array/Solution.java
@@ -193,4 +193,38 @@ public class Solution {
         return Arrays.copyOfRange(res, 0, length);
     }
 
+    /**
+     * 加一
+     *
+     * 给定一个由整数组成的非空数组所表示的非负整数，在该数的基础上加一。
+     * 最高位数字存放在数组的首位， 数组中每个元素只存储一个数字。
+     * 你可以假设除了整数 0 之外，这个整数不会以零开头。
+     * @param digits
+     * @return
+     */
+    public int[] plusOne(int[] digits) {
+        int add = 1;
+        for (int i = digits.length - 1; i >= 0; i--) {
+            if (i < digits.length - 1 && add != 1) {
+                break;
+            }
+            if (digits[i] + add > 9) {
+                digits[i] = 0;
+                add=1;
+            } else {
+                digits[i] = digits[i] + add;
+                add = 0;
+            }
+        }
+        if (add >= 1) {
+            int[] newDigits = new int[digits.length + 1];
+            newDigits[0] = add;
+            for (int i = 1; i < newDigits.length; i++) {
+                newDigits[i] = digits[i - 1];
+            }
+            return newDigits;
+        }
+        return digits;
+    }
+
 }
