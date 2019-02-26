6d8f8bde67579362fa9dac0e9af7c03cac475e17
yongxing.chen <yongxing.chen@qunar.com>
2019-02-26 21:02:38 +0800

    int[] twoSum(int[] nums, int target)

--- src/main/java/com/leetcode/problems/exploreAlgorithm/array/Solution.java
+++ src/main/java/com/leetcode/problems/exploreAlgorithm/array/Solution.java
@@ -1,7 +1,9 @@
 package com.leetcode.problems.exploreAlgorithm.array;
 
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 
 /**
  * @author zrom
@@ -247,6 +249,29 @@ public class Solution {
         for (int i = index; i < nums.length; i++) {
             nums[i] = 0;
         }
+    }
+
+    /**
+     * 两数之和
+     *
+     * 给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。
+     * 你可以假设每种输入只会对应一个答案。但是，你不能重复利用这个数组中同样的元素。
+     * @param nums
+     * @param target
+     * @return 例如：[2, 7, 11, 15] 9, 返回：[2, 7]
+     */
+    public int[] twoSum(int[] nums, int target) {
+        Map map = new HashMap<>(nums.length / 2);
+        for (int i = 0; i < nums.length; i++) {
+            int ans = target - nums[i];
+            Integer ansIndex = (Integer) map.get(ans);
+            if (ansIndex != null) {
+                return new int[] {ansIndex, i};
+            } else {
+                map.put(nums[i], i);
+            }
+        }
+        return null;
     }
 
 }
