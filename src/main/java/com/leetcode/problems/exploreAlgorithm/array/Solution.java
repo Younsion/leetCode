cc4ed7e74db25a1fe1c568a442459b1b1e664164
yongxing.chen <yongxing.chen@qunar.com>
2019-02-26 09:25:34 +0800

    void moveZeroes(int[] nums);

--- src/main/java/com/leetcode/problems/exploreAlgorithm/array/Solution.java
+++ src/main/java/com/leetcode/problems/exploreAlgorithm/array/Solution.java
@@ -227,4 +227,26 @@ public class Solution {
         return digits;
     }
 
+    /**
+     * 移动零
+     *
+     * 给定一个数组 nums，编写一个函数将所有 0 移动到数组的末尾，同时保持非零元素的相对顺序。
+     * @param nums
+     */
+    public void moveZeroes(int[] nums) {
+        if (nums == null || nums.length < 2) {
+            return;
+        }
+        int index = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                nums[index] = nums[i];
+                index++;
+            }
+        }
+        for (int i = index; i < nums.length; i++) {
+            nums[i] = 0;
+        }
+    }
+
 }
